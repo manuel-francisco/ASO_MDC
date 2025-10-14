0634eb2b2aa9b7dd602467409d78ddc6c9f206ad
Jaime Jarque <jaimejarquegutierrez1@gmail.com>
2025-10-14 21:24:45 +0200

    Resolucion del ejercicio 5

---
+++ scripts/01_basics/05_args_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Comprobamos que se han introducido exactamente 2 argumentos
+if ! [ $# == 2 ]; then
+    echo "Uso: ./05_args_check.sh <num1> <num2>"
+    exit 1
+fi
+
+#Comprobamos que ambos argumentos son numéricos
+if ! [[ "$1" =~ ^[0-9]+$ ]] || ! [[ "$2" =~ ^[0-9]+$ ]]; then
+    echo "Ambos argumentos deben ser números."
+    exit 2
+fi
+
+#Si son numeros, entonces los sumamos
+suma=$(($1 + $2))
+
+#Mostramos el resultado
+echo "Resultado: $suma"
