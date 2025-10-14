8568e3e04eac2c03198a1251311a1d9115b1f348
Guille <gsangon2101@g.educaand.es>
2025-10-14 21:40:18 +0200

    fix del ej3

--- scripts/01_basics/03_io.sh
+++ scripts/01_basics/03_io.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -p "dime un número " num1
-num2=$((num1/2))
-echo "$((num2-1)) pues ahora es par"
+read -p "dime un número y te dare uno impar " num1
+num2=$((num1*2))
+echo "$((num2+1)) ves"
