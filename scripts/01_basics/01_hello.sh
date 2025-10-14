858b7de359fc91ea62cbab489ce71bcf6fd0f6b0
Daniela <dmacmac250@g.educaand.es>
2025-10-14 18:21:05 +0200

    Ejercicio F1E1 resuelto

---
+++ scripts/01_basics/01_hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Mostramos el nombre actual
+echo "Mi nombre es $USER"
+#Mostramos el directorio de trabajo actual
+echo "Mi directorio actual es $PWD"
+#Mostramos la fecha y hora del sitema
+echo "La fecha y hora es $(date)"
