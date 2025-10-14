f6e5a1282bc5674cdd13e555a92bdbe7e82475e1
J. Ignacio Montalbán <jmonsar029@g.educaand.es>
2025-10-14 19:49:09 +0200

    Borrado el archivo 02_variables.sh ya que no es correcto en esta rama

--- scripts/01_basics/02_variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NOMBRE="Ignacio"  # $(whoami) para usar el nombre del usuario que ejecuta
-CURSO="Segundo de ASIR"
-FECHA=$(date) # Ejecuta el comando date y lo guarda en la variable FECHA
-
-echo "Soy $NOMBRE de $CURSO."
-echo "Hoy es $FECHA"
-echo "Estoy trabajando en el directorio $(pwd)"
