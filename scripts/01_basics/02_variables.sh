61d4c3a953d3eb5fba4809fe852a8daf9a622704
Pablo <pjodmon0503@g.educaand.es>
2025-10-14 20:29:10 +0200

    Realizado el ejercicio 2

---
+++ scripts/01_basics/02_variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Definiendo las variables
+NOMBRE="$whoami"
+CURSO="Segundo de ASIR"
+FECHA="$(date)"
+
+
+#Utilizamos  las variables con el comando echo
+echo "Soy $NOMBRE de $CURSO"
+echo "Hoy es $FECHA"
+echo "Estoy trabajando en el directorio $(pwd)"
+
+
