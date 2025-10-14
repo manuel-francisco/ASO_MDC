3c2aba5455b646d5ac579c8b917fd6486be98c7b
Angel_Lopez <aloploa084@g.educaand.es>
2025-10-14 20:33:59 +0200

    errores resueltos

--- scripts/01_basics/03_io.sh
+++ scripts/01_basics/03_io.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read "Introduce tu nombre " nombre
-read "Introduce tu edad " edad
+read -p "Introduce tu nombre: " nombre
+read -p "Introduce tu edad: " edad
 
-opera=$((100 - $edad))
+opera=$((100-$edad))
 
 echo "Hola $nombre, te quedan $opera años para cumplir 100"
