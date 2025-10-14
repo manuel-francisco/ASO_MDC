8ca0be6e558be59582fb972c919b7dee7dcf9176
paquito <rotocoplador@cuantico.asir>
2025-10-14 20:54:00 +0200

    Ejercicio 2 Resuelto

--- scripts/01_basics/02_variables.sh
+++ scripts/01_basics/02_variables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# Declaramos las variables
 NOMBRE="Antonio"
 CURSO="Segundo de ASIR"
 
+# Muestro mi nombre y mi curso.
 echo "Soy $NOMBRE de $CURSO"
+
+# Muestro la fecha.
 echo "Hoy es $(date)"
+
+# Muestro desde que directorio estoy trabajando.
 echo "Estoy trabajando en el directorio $(pwd)"
