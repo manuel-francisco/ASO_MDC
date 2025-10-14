55b0a858d4be697e9622d6019ad3c0969d4f7f7e
paquito <rotocoplador@cuantico.asir>
2025-10-14 21:22:50 +0200

    Ejercicio 3 Resuelto

---
+++ scripts/01_basics/03_io.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Uso de read para obtener las variables.
+echo "Introduzca su nombre: "
+	read NOMBRE
+echo "Introduzca su edad: "
+	read EDAD
+
+# Confirmación de que la edad introducida es válida.
+
+if [["$EDAD" =^[0-9]+$]]; then
+		echo "Esa edad no es válida."
+		exit 
+	else
+		echo "La edad es válida"
+fi 
+
+# Calculo para saber cuantos años le quedan para cumplir 100.
+Queda=$((100 - $EDAD))
+
+echo "Hola $NOMBRE, le quedan $Queda años para tener 100 años"
