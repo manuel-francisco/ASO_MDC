1420e4e622ddf328c2712a0841cc18564c26ed7c
Gueri <diegogueri26@gmail.com>
2025-10-14 19:38:40 +0200

    F1E2 - Variables y entorno

---
+++ scripts/01_basics/02_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#variables
+NOMBRE=$(whoami)
+CURSO="Segundo de ASIR"
+FECHA=$(date)
+
+#Comandos
+
+echo "Soy $NOMBRE de $CURSO."
+echo "Hoy es $FECHA."
+echo "Estoy trabajando en el directorio $(pwd)."
