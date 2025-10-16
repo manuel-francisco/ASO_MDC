a8f5da96cb35eaffb3692de8492d4f5ebe907e63
Manuel Francisco <117926750+manuel-francisco@users.noreply.github.com>
2025-10-16 18:07:59 +0000

    Removed LUA filter

--- needspace.lua
+++
@@ -1,8 +0,0 @@
-function RawBlock(el)
-  if el.format == "latex" and el.text:match("\\begin{longtable}") then
-    return {
-      pandoc.RawBlock("latex", "\\needspace{10\\baselineskip}"),
-      el
-    }
-  end
-end
