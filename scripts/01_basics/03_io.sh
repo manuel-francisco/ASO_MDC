d1afe4891eb3bd5d3732cb7dcc777bf654f1611c
Pablo <pjodmon0503@g.educaand.es>
2025-10-16 21:19:58 +0200

    Realizado el ejercicio 3

--- scripts/01_basics/03_io.sh
+++ scripts/01_basics/03_io.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-read -p "Introduce tu nombre" nombre
-read -p "Introduce tu edad" edad
+read -p "Introduce tu nombre: " nombre
+read -p "Introduce tu edad: " edad
 
 
-edadfinal=$((100-$edad))
-
-if [[ -z "$edad" ]];
-then
-	echo "La variable está vacía"
-elif [[ $edadfinal =~ [^0-9]+$ ]];
-then
-	echo "La variable no es un numero"
+if [[ -z "$edad" ]]; then
+    echo "La variable está vacía"
+elif ! [[ "$edad" =~ ^[0-9]+$ ]]; then
+    echo "La variable no es un número"
 else
-	echo "Hola $nombre, te quedan $edadfinal años para cumplir 100."
+    edadfinal=$((100 - $edad))
+    echo "Hola $nombre, te quedan $edadfinal años para cumplir 100."
 fi
-
