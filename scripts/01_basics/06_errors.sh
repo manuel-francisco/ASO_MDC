40dc197a876941e5c8db06bb30dfbde547fa623f
Angel_Lopez <aloploa084@g.educaand.es>
2025-10-17 18:35:11 +0200

    F1E6 hecho

---
+++ scripts/01_basics/06_errors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#num=2
+#if [num-eq2]; then
+
+#echo "El if-else funciona"
+
+#fi
+
+#echo "$saludo"
+
+#ls /etc/hola
+
+
+num=2
+if [ $num -eq 2 ]; then
+
+echo "El if-else funciona"
+
+fi
+
+saludo="hola"
+echo "$saludo"
+
+ls /etc/apt
