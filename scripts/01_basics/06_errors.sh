cd3ed4eac6325637ec6dfa66354e3f31ffdd3e4b
Gueri <diegogueri26@gmail.com>
2025-10-17 18:28:10 +0200

    F1E6 - Depuración_Diegogueri

---
+++ scripts/01_basics/06_errors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+var=1
+
+
+
+#if [[$var -eq 1]];then
+#echo "hola"
+
+#fi
+#echo $numero
+
+#ls /pepe
+
+
+if [[ $var -eq 1 ]];then
+echo "hola"
+fi
+
+numero=2
+
+echo $numero
+
+
+ls
