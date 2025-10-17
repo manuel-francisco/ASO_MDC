da9b46204999b3cb6aca9183fb0d02cbbb12b9cf
fsan <fsanrui2110b@g.educaand.es>
2025-10-17 12:36:45 +0200

    Resolución del F1E3

---
+++ scripts/01_basics/03_io.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# pregunta el nombre y edad
+read -p "Escriba su nombre: " name
+read -p "Escriba su edad: " age
+
+# comprueba que los datos no estén vacíos
+if [[ -z $name || -z $age ]]; then
+	echo "Rellene ambos valores."
+	exit 1
+fi
+
+# comprueba que $edad sea un número
+if [[ $age =~ ^[0-9]+$ ]]; then
+	# comprueba si su edad ya llegó a los 100
+	if [[ $age -ge 100 ]]; then
+		echo "Felicidades $name, usted ya es un centenario con sus $age años."
+	else
+		# calcula los años restantes para 100
+		year=$((100-age))
+		echo "Buenas tardes $name, le quedan $year año(s) para llegar a su centenario."
+	fi
+else
+	echo "Escriba un número en el campo edad."
+fi
+
+
