7acc60ef5fb64bec7472d1d025bd3c964f657fd9
Jaime <jaimejarquegutierrez1@gmail.com>
2025-10-21 18:34:45 +0000

    Comentar script

--- scripts/03_functions/01_simple_function.sh
+++ scripts/03_functions/01_simple_function.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Definimos la funcion saludo
 saludo () {
+    # Imprimimos por pantalla el siguiente mensaje.
     echo "¡Hola $(whoami)! Bienvenido al mundo de Bash."
 }
 
+# Llamamos a la funcion para que se ejecute
 saludo
 
+# Se añade un mensaje para controlar el flujo de la ejecucion
 echo "Que tengas un buen dia"
