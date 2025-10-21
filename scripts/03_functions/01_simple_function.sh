fcd64f8a5d4a0b56b17d08bb30e314db8da9445c
Gueri <diegogueri26@gmail.com>
2025-10-21 17:32:07 +0200

    F3E1 - Función simple Diegogueri

---
+++ scripts/03_functions/01_simple_function.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#crear la funcion
+function saludo {
+#contenido de la funcion
+echo "¡Hola, $(whoami)! Bienvenido al mundo de Bash."
+
+}
+
+
+echo "---------"
+#llamar a la funcion
+saludo 
+
+
+
+echo "---------"
