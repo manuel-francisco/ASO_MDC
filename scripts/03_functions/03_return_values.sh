954357c2949c02b90394d4124d5fb80d70c31598
DanielaMacMac06 <danielamacmac06@gmail.com>
2025-10-21 18:12:39 +0000

    Ejercicio F3E3 Resuelto

--- scripts/03_functions/03_return_values.sh
+++ scripts/03_functions/03_return_values.sh
@@ -1,1 +1,24 @@
-fdsknfjksdfj
+#!/bin/bash
+
+es_par() {
+
+if [[ $(( $1 % 2 )) -eq 0 ]]; then
+    return 0
+else
+    return 1
+fi
+
+}
+
+
+read -p "Escribe un numero: " NUM
+
+es_par $NUM
+
+if [[ $? -eq 0 ]] 
+then
+echo "El numero $NUM es par"
+else
+echo "El numero $NUM es impar"
+fi
+
