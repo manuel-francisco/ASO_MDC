bb7b58be315eb83c21cf110644470bd901b1a674
Diegogueri <diegogueri26@gmail.com>
2025-10-21 17:32:12 +0000

    F3E3 - Retorno Diegogueri

--- scripts/03_functions/03_return_values.sh
+++ scripts/03_functions/03_return_values.sh
@@ -1,1 +1,30 @@
-hjh
+#!/bin/bash
+#se crea la funcion
+function es_par {
+
+
+#estructura if
+if (( $1 % 2 == 0 )); then
+    return 0
+else
+    return 1
+fi
+
+}
+
+read -p "Introduce un numero: " num
+
+es_par $num
+
+if [[ $? -eq 0 ]];then
+#Llama a la funcion
+echo "$num es par"
+
+
+else
+
+echo "$num es impar"
+
+fi
+
+#retunr detiene la ejecucion de la funcion y envia un codigo de salida al proceso que la llamó.
