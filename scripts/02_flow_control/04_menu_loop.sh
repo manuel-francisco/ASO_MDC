86ccf7b2911143a7b1db4ac9549e75c9d6aa915f
Guille <gsangon2101@g.educaand.es>
2025-10-21 20:15:33 +0200

    F2E4

---
+++ scripts/02_flow_control/04_menu_loop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Script que muestra un menú básico y ejecuta una acción según la opción elegida usando case y se repite mediante un while
+
+option=1
+
+while [[ "$option" -ne 4 ]]; do
+
+# Mostrar el menú de opciones
+
+echo -e "1) Mostrar fecha actual \n2) Mostrar usuario actual \n3) Mostrar directorio de trabajo \n4) Salir"
+
+# Pedir al usuario que introduzca una opción
+read -p "elija una opción " option
+# Evaluar la opción con estructura case
+case $option in
+  1)
+    # Mostrar la fecha actual
+    echo "Fecha actual: $(date)"
+    ;;
+  2)
+    # Mostrar el usuario actual
+    echo "Usuario actual: $(whoami)"
+    ;;
+  3)
+    # Mostrar  el directorio
+    echo "Directorio actual: $(pwd)"
+    ;;
+  4)
+    # Mensaje de despedida y finalización del script
+    echo "Saliendo. ¡Hasta luego!"
+    exit 0
+    ;;
+  *)
+    # Caso por defecto para opciones no válidas
+    echo "Opción no válida"
+    ;;
+esac
+
+done
