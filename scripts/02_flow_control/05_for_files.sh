6a2b890b651e665843a34b0e8352039ff62ebeed
Pablo <pjodmon0503@g.educaand.es>
2025-10-24 16:24:53 +0200

    Realizado el ejercicio 5 del apartado 2

--- scripts/02_flow_control/05_for_files.sh
+++ scripts/02_flow_control/05_for_files.sh
@@ -6,7 +6,7 @@ for FILE in *; do
 
 	#archivo normal y corriente. stat -c%s lo que hace es calcular el tamaño en bytes del archivo que vamos a comprobar
 if [[ -f "$FILE" ]]; then
-TAMANYO=$(stat -c%s "$FILE")
+TAMANYO=$((stat -c%s "$FILE"))
 	echo "Nombre del fichero: $FILE"
 	echo "Tamaño: Ocupa $TAMANYO bytes"
 	#está puesto como "tamanyo" para que bash no se confunda por la ñ al definir la variable, por si acaso
