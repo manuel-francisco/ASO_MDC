9e48770bd0ea3c60745863000479b5970eea171f
Miguel Angel Garcia <mgarcua1612@g.educaand.es>
2025-10-24 18:03:16 +0200

    F33E5 - Biblioteca de funciones miguel11520

---
+++ scripts/03_functions/05_main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source 0/05_library.sh
+
+echo "=== Utilidades del sistema ==="
+echo "1) Mostrar fecha"
+echo "2) Mostrar usuario"
+echo "3) Mostrar espacio en disco"
+echo "4) Salir"
+
+read -p "Selecciona una opcion: " OPCION
+
+case $OPCION in
+	1) mostrar_fecha ;;
+	2) mostrar_usuario ;;
+	3) mostrar_espacio ;;
+	4) echo "Saliendo..."; exit 0 ;;
+	*) echo "Opción no válida." ;;
+esac
