0e57534b29b0a0ebdebf8454d181e80cdc9dd37d
Guille <gsangon2101@g.educaand.es>
2025-10-24 16:38:36 +0200

    F2E6

---
+++ scripts/02_flow_control/06_while_counter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Solicita al usuario que introduzca un número y lo guarda en la variable N
+read -p "Introduce un número > " N
+
+# Inicializa la variable fakeN en 1 para usarla como contador
+fakeN=1
+
+# Imprime el primer número (1) antes del bucle
+echo "1"
+
+# Mientras el valor de fakeN sea diferente al de N, se ejecuta el bucle
+while [[ fakeN -ne N ]]; do
+  # Incrementa el contador fakeN en 1
+  fakeN=$((fakeN+1))
+ 
+  # Imprime el valor actual de fakeN
+  echo "$fakeN"
+done
+
+# Cuando finaliza el bucle, indica que se ha terminado y muestra cuántos números se imprimieron
+echo "Contador finalizad. Se han mostrado $N números"
