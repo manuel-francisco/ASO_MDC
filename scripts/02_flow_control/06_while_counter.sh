9b5a00f5e849adef7f4ea8c4662d2a41ac866d40
Pedro <pmurpat044@g.educaand.es>
2025-11-03 04:14:48 +0100

    F2E6 – Bucle while contador PedroDMurillo

--- scripts/02_flow_control/06_while_counter.sh
+++ scripts/02_flow_control/06_while_counter.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-
+#Esta linea pide al usuario el numero entero y lo almacena en la variable N
 read -p "Introduce un numero entero: " N
+
+#Esto empieza la cuenta en 1 
 COUNT=1
 
+#Bucle while, para que se cumpla la condicion, hasta que la cuenta sea menor que el valor introducido
 while [[ $COUNT -le $N ]]; do
 
+#Muestra el contador, numero por numero
 echo $COUNT
 COUNT=$((COUNT+1))
 
+#Añade una pequeña pausa durante la ejecucion del script
 sleep 1
 
 done
