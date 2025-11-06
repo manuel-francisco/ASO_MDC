ecccfdb9d1f8ea76dc1b45344654b29201c24106
T0lsan <htolsan1105@g.educaand.es>
2025-11-06 17:48:53 +0100

    Ahora si pones un valor no numerico, te dice que esta mal

--- scripts/02_flow_control/06_while_counter.sh
+++ scripts/02_flow_control/06_while_counter.sh
@@ -2,6 +2,9 @@
 
 read -p "Introduce un número > " contar
 
+#Si contar es un valor numerico, se realiza el bucle
+if [[ "$contar" =~ ^[0-9]+$ ]]; then
+
 #Mientras COUNT no sea igual a la variable contar va a seguir el bucle.
 while [[ COUNT -ne contar ]]; do
 
@@ -12,3 +15,12 @@ done
 
 #Una vez finalize el bucle pone este echo al final
 echo "Contador finalizado. Se han mostrado $COUNT números"
+exit 0
+
+else
+
+#Si pones cualquier otra cosa que no sea un número te pone lo siguiente:
+echo "!!Payo eso no es un número"
+exit 1
+
+fi
