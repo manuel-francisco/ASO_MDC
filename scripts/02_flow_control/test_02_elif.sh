63eccf68390b4bf69dcca08c059eeeb6dc29b457
J Ignacio M <jmonsar029@g.educaand.es>
2025-11-10 13:50:51 +0100

    test_02_elif.sh  modificado para usar expansion indirecta, corrigiendo un error en la ejecución de las funciones. El test se sube con una comprobación erronea para comprobar el correcto funcionamiento de 'ejecucion_test.yml'

--- scripts/02_flow_control/test_02_elif.sh
+++ scripts/02_flow_control/test_02_elif.sh
@@ -6,7 +6,7 @@ TEST2=$(echo -e "5\n" | ./02_elif.sh)
 TEST3=$(echo -e "6\n" | ./02_elif.sh)
 TEST4=$(echo -e "8\n" | ./02_elif.sh)
 TEST5=$(echo -e "9\n" | ./02_elif.sh)
-TEST6=$(echo -e "12\n" | ./02_elif.sh)
+TEST6=$(echo -e " 2\n" | ./02_elif.sh)
 
 # Mensajes que debe de aparecer en función del número introducido, nos aseguramos que 
 # los test y los mensajes van acordes.
@@ -25,11 +25,19 @@ if [[ "$1" ==  "$2" ]];then
     echo "TEST OK"
 else
     echo "TEST FAILED"
+    exit 1
 fi 
 }
 
 # Iteramos mediante un bucle para no tener que escribir la función para cada test
 for i in {1..6};do
-    funcion_test "$TEST${i}" "$MENSAJE${i}"
+    # Guardamos en una nueva variable el nombre de la variable que contiene el valor que queremos
+    # Esto es lo mismo que escribir TEST1 y MENSAJE1 en la primera iteración
+    variable_test="TEST${i}"
+    variable_mensaje="MENSAJE${i}"
+    # Realizamos una expansión indirecta con ${!texto} lo cual expande el valor dentro de la variable 
+    # expandida, en este caso variable_test expande a TEST1 y como TEST1 es una variable y tiene un valor,
+    # expandimos al valor de esa variable
+    funcion_test "${!variable_test}" "${!variable_mensaje}"
 done
 
