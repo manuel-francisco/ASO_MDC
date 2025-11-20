843751a489dffaf2463169b1b427981f6b032933
Pablo <pjodmon0503@g.educaand.es>
2025-11-20 17:44:56 +0100

    añadido ejercicio actualizado y test actualizado

--- scripts/tests/02_flow_control/test_01_if_else.sh
+++ scripts/tests/02_flow_control/test_01_if_else.sh
@@ -2,7 +2,7 @@
 
 SALIDA=$(echo -e "11\n" | ./01_if_else.sh)
 
-if [[ "$SALIDA" == *es mayor* ]]; then
+if [[ "$SALIDA" == "11 es mayor que 10" ]]; then
     echo -e "[MAYOR]\tTEST OK"
 else
     echo -e "[MAYOR]\tTEST FAILED"
@@ -14,7 +14,7 @@ fi
 
 SALIDA=$(echo -e "9\n" | ./01_if_else.sh)
 
-if [[ "$SALIDA" == *no es mayor* ]]; then
+if [[ "$SALIDA" == "9 es menor que 10" ]]; then
     echo -e "[MENOR]\tTEST OK"
 else
     echo -e "[MENOR]\tTEST FAILED"
