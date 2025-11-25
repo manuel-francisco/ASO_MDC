53a363fe9d936ba4b4a84bcf64b5728725f4e92e
Pablo <pjodmon0503@g.educaand.es>
2025-11-25 20:57:13 +0100

    actualizado

--- scripts/tests/02_flow_control/test_01_if_else.sh
+++ scripts/tests/02_flow_control/test_01_if_else.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
+source "scripts/test/base_test.sh"
+SCRIPT="scripts/02_flow_control/01_if_else.sh"
 
-SALIDA=$(echo -e "11\n" | ./01_if_else.sh)
+test_case "numero >10" \
+	"echo -ne \"20\n\" | $SCRIPT" \
+	"El numero 20 es mayor que 10" \
+	0
 
-if [[ "$SALIDA" == "11 es mayor que 10" ]]; then
-    echo -e "[MAYOR]\tTEST OK"
-else
-    echo -e "[MAYOR]\tTEST FAILED"
-    echo "Salida obtenida: $SALIDA"
-    exit 1
-fi
+test_case "numero igual 10" \
+	"echo -ne \"10\n\" | $SCRIPT" \
+	"Tu numero es 10" \
+	0
 
+test_case "numero <10" \
+	"echo -ne \"5\n\" | $SCRIPT" \
+	"El numero 5 es menor que 10" \
+	0
 
-
-SALIDA=$(echo -e "9\n" | ./01_if_else.sh)
-
-if [[ "$SALIDA" == "9 es menor que 10" ]]; then
-    echo -e "[MENOR]\tTEST OK"
-else
-    echo -e "[MENOR]\tTEST FAILED"
-    echo "Salida obtenida: $SALIDA"
-    exit 1
-fi
+summary || exit 1
