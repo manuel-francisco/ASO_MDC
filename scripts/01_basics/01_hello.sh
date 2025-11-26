f580c986a1137cb50a32e43edb4e133bf068d954
tenowp <jesustenoriolopez@gmail.com>
2025-11-26 11:07:26 +0100

    Hola, Bash Tenowp

--- scripts/01_basics/01_hello.sh
+++ scripts/01_basics/01_hello.sh
@@ -3,7 +3,7 @@
 
 #Mostrar el nombre actual
 echo "Mi nombre es $USER"
-#Mostrar el directorio actual
+#Mostrar el directorio de trabajo actual
 echo "Mi directorio actual es $pwd"
 #Mostrar la fecha y hora actual
 echo "La fecha y hora es $(date)"
