884e8f12694235a19f07dc3828af47624abd2428
tenowp <jesustenoriolopez@gmail.com>
2025-11-26 10:42:04 +0100

    Ejercicio uno de la primera relacion terminado

---
+++ scripts/01_basics/01_hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Mostrar el nombre actual
+echo "Mi nombre es $USER"
+#Mostrar el directorio actual
+echo "Mi directorio actual es $pwd"
+#Mostrar la fecha y hora actual
+echo "La fecha y hora es $(date)"
