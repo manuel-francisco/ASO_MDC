13597fe74a45c3e193b2d15badce6999814d089a
tenowp <jesustenoriolopez@gmail.com>
2025-11-27 12:54:42 +0100

    Ejercicio 6 terminado

---
+++ scripts/02_flow_control/01_if_else.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Pide por pantalla que introduzca un número
+read -p "Introduce un número entero: " num
+#El número es menor que 10
+if [[ $num -lt 10 ]] then
+	echo "El $num es menor que 10"
+exit 0
+#El número es mayor que 10
+if [[ $num -gt 10 ]] then
+	echo "El $num es menor que 10"
+exit 0
+#Los dos números son iguales
+else
+	echo "Los números son iguales"
+fi
