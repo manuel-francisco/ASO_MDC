dc4d28f4053f511fb6e63b0b8b0c38b69f15217a
J Ignacio M <jmonsar029@g.educaand.es>
2025-11-30 16:06:29 +0100

    Automatizacion-Script-ajimontalban

--- scripts/05_automation/backup.sh
+++ scripts/05_automation/backup.sh
@@ -2,24 +2,49 @@
 
 # suponemos que tomamos como valor de entrada el fichero usuarios.csv
 
+LOG="/var/log/gestion_usuarios.log"
+ELIMINADOS="/home/.eliminados"
 
-echo "usuario,grupo,operacion" > usuarios.csv
+if ! [[ -d $ELIMINADOS ]]; then
+    mkdir -p $ELIMINADOS
+fi
 
-echo "Pedro,asir,add" >> usuarios.csv
-echo "Angel,asir,add" >> usuarios.csv
-echo "Pedro,asir,rm" >> usuarios.csv
-echo "Angel,asir,rm" >> usuarios.csv
-
-function annadir_usuario(){
-    useradd -aG "$2" "$1"
-    if [[ "$?" != 0  ]];then
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Error!: No se ha podido añadir el usuario $1 al grupo $2"
-        
-    else
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] - operación: $3 - usuario afectado: $1" 
-    fi
-
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - operación: $3 - usuario afectado: $1" 
-
-
+function log(){
+    echo "$(date) - $1 - $2 $3" >> "$LOG"
 }
+
+while IFS=',' read -r usuario grupo operacion; do
+    case $operacion in
+        "add")
+            if grep -q "^$usuario:" /etc/passwd ;then
+                log $operacion "El usuario ya existe:" $usuario
+                continue
+            else
+                if grep -q "^$grupo:" /etc/group ;then
+                    log $operacion "El grupo ya existe:" $grupo
+                else
+                    groupadd "$grupo"
+                    log $operacion "creado el grupo" $grupo
+                fi
+                useradd -m -d "/home/$usuario" -g "$grupo" "$usuario"
+                log $operacion "creado el usuario: $usuario"
+            fi
+            ;;
+
+        "rm")
+            if ! grep -q "^$usuario:" /etc/passwd;then
+                log $operacion "No existe el usuario: $usuario"
+                continue
+            elif passwd -S "$usuario" | grep -q "L";then
+                log $operacion "El usuario ya esta bloqueado: $usuario"
+                continue
+            else
+                usermod -L $usuario
+                mv "/home/$usuario" "$ELIMINADOS"
+                log $operacion "El usuario ha sido bloqueado: $usuario"
+            fi
+            ;;
+    esac
+done < "usuarios.csv"
+
+
