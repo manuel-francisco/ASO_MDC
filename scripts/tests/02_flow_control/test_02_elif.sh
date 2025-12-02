17f9d3599ff082043c28bac782fd75fa49c3224c
J. Ignacio Montalbán <jmonsar029@g.educaand.es>
2025-12-02 21:45:17 +0100

    FT2E2-elif-ajimontalban

---
+++ scripts/tests/02_flow_control/test_02_elif.sh
@@ -0,0 +1,24 @@
+#/bin/bash 
+
+source "scripts/tests/base_test.sh"
+SCRIPT="scripts/02_flow_control/02_elif.sh"
+
+test_case "Comprobación número fuera de rango" \
+    "echo -e \"12\" | $SCRIPT" \
+    "El número indicado está fuera del rango de notas." \
+    1
+
+test_case "suspenso" \
+    "echo -e \"2\" | $SCRIPT" \
+    "Tu calificación es: Suspenso" \
+    0
+
+test_case "Aprobado" \
+    "echo -e \"5\" | $SCRIPT" \
+    "Tu calificación es: Aprobado" \
+    0
+
+test_case "Bien" \
+    "echo -e \"6\" | $SCRIPT" \
+    "Tu calificación es: Bien" \
+    0
